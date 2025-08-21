c3cae77dfe8bada5bb5cfbafeca6e018d9679e28
陈奕宏 <465126135@qq.com>
2025-08-22 02:32:31 +0800

    init

---
+++ src/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Litenbib.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
