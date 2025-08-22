6960c0beb17c33d1f7862e43ce5fb8619daa0aae
陈奕宏 <465126135@qq.com>
2025-08-22 20:33:34 +0800

    basic window

--- src/Views/MainWindow.axaml.cs
+++ src/Views/MainWindow.axaml.cs
@@ -1,4 +1,10 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Platform;
+using System;
+using System.Globalization;
 
 namespace Litenbib.Views
 {
@@ -7,6 +13,41 @@ namespace Litenbib.Views
         public MainWindow()
         {
             InitializeComponent();
+            this.PropertyChanged += OnPropertyChanged;
+        }
+
+        private void OnPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+        {
+        
+        }
+
+        private void TitleButton_Click(object? sender, RoutedEventArgs e)
+        {
+            if (e.Source is not Button button) { return; }
+            switch (button.Name)
+            {
+                case null:
+                    return;
+                case "MinButton":
+                    { WindowState = WindowState.Minimized; }
+                    break;
+                case "MaxButton":
+                    {
+                        if (WindowState == WindowState.Maximized)
+                        { WindowState = WindowState.Normal; }
+                        else
+                        { WindowState = WindowState.Maximized; }
+                    }
+                    break;
+                case "CloseButton":
+                    { Close(); }
+                    break;
+            }
+        }
+
+        private void TitleBar_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+            this.BeginMoveDrag(e);
         }
     }
 }
